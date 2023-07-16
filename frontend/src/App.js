e4bf6055b2eac89154455370eebbe27b8293c685
Apurba Debnath <62664361+apurba2002@users.noreply.github.com>
2023-07-16 11:30:04 +0600

    hhjhj

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Pink from './pink';
+
+
+function App() {
+  console.log(document.getElementById('pink'))
+  return (
+    <>
+         <Pink />
+    </>
+  );
+}
+
+export default App;
